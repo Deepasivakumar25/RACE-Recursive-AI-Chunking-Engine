9c7d6974a6d46e41a31c7b2d5f19923c23c316c6
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-08-31 15:45:06 +0530

    Delete src/02_pdf_extraction.py

--- src/02_pdf_extraction.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader
-
-
-def extract_pdf_text(pdf_path: str) -> str:
-    reader = PdfReader(pdf_path)
-    rec_pdf = ""
-    for rec in reader.pages:
-        rec_pdf += rec.extract_text() or ""
-    return rec_pdf
